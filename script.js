f0e47fdff39a02d21e6d16913513d32e09186f90
ImVeris <141730322+ImVeris@users.noreply.github.com>
2023-10-05 17:26:42 +0200

    Added logic of the game.

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ const playerFactory = (name, mark) => {
 const Game = (() => {
     let players = [];
     let currentPlayerIndex;
-    let isFinished;
+    let isFinished = false;
 
     const start = () => {
         players = [
@@ -47,17 +47,73 @@ const Game = (() => {
     }
 
     const handleClick = (event) => {
+        if(isFinished){
+            return;
+        }
         let index = parseInt(event.target.className.slice(-1));
         if (gameboard.getGameboard()[index] !== '')
             return;
         gameboard.update(index, players[currentPlayerIndex].mark);
         gameboard.render();
 
+        if(checkForWin(gameboard.getGameboard(), players[currentPlayerIndex].mark)){
+            displayController.renderMessage(`${players[currentPlayerIndex].name} wins!`);
+            isFinished = true;
+        } else if(checkForTie(gameboard.getGameboard())){
+            displayController.renderMessage("Tie!")
+            isFinished = true;
+        }
+
         currentPlayerIndex = currentPlayerIndex === 1 ? 0 : 1;
     }
 
-    return { start, handleClick }
+    const restart = () => {
+        for (let i = 0; i < 9; i++) {
+            gameboard.update(i,'');
+        }
+        gameboard.render();
+        isFinished  = false;
+        currentPlayerIndex = 0;
+        displayController.renderMessage("");
+    }
+
+    const checkForWin = (board) =>{
+        const winningCombinations = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
+        for(let i =0; i < winningCombinations.length;i++){
+            const [a,b,c] = winningCombinations[i]
+            if(board[a] && board[a] === board[b] && board[a] === board[c]){
+                return true;
+            }
+        }
+    }
+
+    const checkForTie = (board) =>{
+        return board.every(cell => cell !== "")
+    }
+
+    return { start, handleClick, restart }
+})()
+
+const displayController = (() =>{
+    const renderMessage= (message) => {
+        document.querySelector('.winner-msg').innerHTML = message;
+    }
+    return{
+        renderMessage
+    }
 })()
 
 const startBtn = document.querySelector('.start-btn');
 startBtn.addEventListener('click', () => Game.start())
+
+const restartBtn = document.querySelector('.restart-btn');
+restartBtn.addEventListener('click', ()=> Game.restart())
